090a720ba290b6bfe9eaadd0255b5b600aff032c
101622926 <101622926@TC113B-014>
2018-01-31 16:06:12 +1100

    Add project files.

---
+++ ClassLibrary1/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> Users = new List<User> { new User("Bill",36) };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataGrid.ItemsSource = Users;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Users.Add(new User(NameInput.Text, int.Parse(AgeInput.Text)));
+            DataGrid.ItemsSource = null;
+            DataGrid.ItemsSource = Users;
+        }
+    }
+}
